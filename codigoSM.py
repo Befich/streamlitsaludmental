239f9b751394da74818029798e418cefb5ad7824
Befich <samuelcortez454@gmail.com>
2024-11-12 16:10:02 -0300

    Update codigoSM.py

--- codigoSM.py
+++ codigoSM.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+
+st.markdown("""
+<style>
+    [data-testid=stSidebar] {
+        background-color: #ff000050;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+with st.sidebar:
+    st.markdown('<h1 style="color: white;">Este es un texto rojo</h1>', unsafe_allow_html=True)
