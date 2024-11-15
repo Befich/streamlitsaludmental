7d5d96da93a4eb6488a4d811057e036aaddf044e
Befich <samuelcortez454@gmail.com>
2024-11-15 12:12:25 -0300

    Update codigoSM.py

--- codigoSM.py
+++ codigoSM.py
@@ -13,7 +13,7 @@ with st.sidebar:
     st.markdown("<h1 style='color: white'>Este es un texto rojo</h1>", unsafe_allow_html=True)
     boton1 = st.button("¿Cual es la relacion de nivel de  estres y modo de trabajo?")
     if boton1:
-        st.write("alo?)
+        st.write("alo")
     casil1= st.checkbox("Hola")
     if casil1:
         st.write("Casilla presionada")
