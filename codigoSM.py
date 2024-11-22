83e8e82477ace575df903f1320d39c1629eb1d5b
Befich <samuelcortez454@gmail.com>
2024-11-22 11:51:54 -0300

    Update codigoSM.py

--- codigoSM.py
+++ codigoSM.py
@@ -12,10 +12,8 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 with st.sidebar:
-    st.header("<h1 style='color: white'>Opciones de color</h1>", unsafe_allow_html=True('opciones de color')
-    color_grafico = st.color_picker('Selecciona un color para el grafico','#007bff')
-    
-    
+    st.header("<h1 style='color: white'>Opciones de color</h1>", unsafe_allow_html=True)
+    color_grafico = st.color_picker('Selecciona un color para el grafico','#007bff')    
     boton1 = st.button("¿Cual es la relacion de nivel de  estres y modo de trabajo?")
     if boton1:
         st.write("alo")
